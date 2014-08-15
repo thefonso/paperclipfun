fe7fa6c58d580775b2e2206b8623f98b1162de90
Alfonso Rush <alephmage@gmail.com>
2014-08-15 00:11:16 -0700

    Update photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,12 +5,15 @@ class Photo
 	field :date, type: DateTime
 
 	belongs_to :user
-	has_mongoid_attached_file :image, 
-   :styles => {
-      :original => ['1920x1680>', :jpg],
-      :small    => ['100x100#',   :jpg],
-      :medium   => ['250x250',    :jpg],
-      :large    => ['500x500>',   :jpg]
-    }
+	has_mongoid_attached_file :image
+   #
+   # you will need - brew install imagemagick for these
+   #  
+   # :styles => {
+   #    :original => ['1920x1680>', :jpg],
+   #    :small    => ['100x100#',   :jpg],
+   #    :medium   => ['250x250',    :jpg],
+   #    :large    => ['500x500>',   :jpg]
+   #  }
 	validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
